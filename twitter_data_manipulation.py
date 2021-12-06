43c5c0e659bb823d248366818760fd67effee3ec
Jakub Żmujdzin <jzmujdzin@gmail.com>
2021-12-06 23:32:45 +0100

    add vader and bert classification

--- twitter_data_manipulation.py
+++ twitter_data_manipulation.py
@@ -3,6 +3,8 @@ import pandas as pd
 import nltk
 import string
 import re
+from transformers import pipeline
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
 class TwitterData:
@@ -10,8 +12,9 @@ class TwitterData:
         self.file_tuple = file_tuple
         self.language = language
         self.stopwords = nltk.corpus.stopwords.words('english')
-        self.stemmer = nltk.PorterStemmer()
         self.lemmatizer = nltk.WordNetLemmatizer()
+        self.bert_classifier = pipeline('sentiment-analysis')
+        self.vader_classifier = SentimentIntensityAnalyzer()
         self.twitter_data = self.get_processed_data()
 
     def files_to_df(self):
@@ -27,17 +30,19 @@ class TwitterData:
 
     def cleanup_data(self, tweet_content):
         return [self.lemmatizer.lemmatize(word) for word in
-                [self.stemmer.stem(word) for word in
                 [word for word in
                  re.split('\W+', re.sub('[0-9]+', '',
                                         ''.join([char for char in re.sub('((www.[^s]+)|(https?://[^s]+))', ' ', tweet_content)
-                                                 if char not in string.punctuation]))) if word not in self.stopwords]]]
+                                                 if char not in string.punctuation]))) if word not in self.stopwords]]
 
     def get_processed_data(self):
         df = self.files_to_df()
         df['processed_content'] = df['content'].apply(lambda x: self.cleanup_data(x))
+        df['bert'] = df['content'].apply(lambda x: self.bert_classifier(x)[0])
+        df = pd.concat([df, pd.DataFrame.from_records(df['bert'])], axis=1)
+        df = df.drop(['bert'], axis=1)
+        df['vader'] = df['content'].apply(lambda review: self.vader_classifier.polarity_scores(str(review)))
         return df
-
 
     @staticmethod
     def get_columns():
@@ -46,5 +51,7 @@ class TwitterData:
 
 if __name__ == '__main__':
     dane = TwitterData((r'E:\bachelor\btc_01_07.json', r'E:\bachelor\btc_07_14.json', r'E:\bachelor\btc_14_21.json', r'E:\bachelor\btc_21_31.json'), 'en')
+    #dane = TwitterData((r'E:\bachelor\text-query-tweets.json', r'E:\bachelor\text-query-tweets1.json'), 'en')
+
 
 
